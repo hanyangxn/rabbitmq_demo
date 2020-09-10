225c66621c968e2a96456b8d62cfb36c847b3330
hanyangxn <m18110024862@163.com>
2020-09-10 17:13:19 +0800

    生产端

---
+++ src/test/java/com/hy/rabbitmq_demo/RabbitmqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hy.rabbitmq_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
